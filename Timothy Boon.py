40aab671b1fe6cc18d93e141aee94da432f71b42
ritz54 <161623178+RitamDutta54@users.noreply.github.com>
2024-03-14 14:33:17 +0530

    Add files via upload

---
+++ Timothy Boon.py
@@ -0,0 +1,15 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Admin
+#
+# Created:     14-03-2024
+# Copyright:   (c) Admin 2024
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+#Timothy Boon
+fname=input("")
+lname=input("")
+print("Hello",lname,fname)
