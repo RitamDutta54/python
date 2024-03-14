7dfc94e9ab61e5274d5c5aa35323a3ac89a13e6b
ritz54 <161623178+RitamDutta54@users.noreply.github.com>
2024-03-14 14:20:23 +0530

    Add files via upload

---
+++ Electricity officer.py
@@ -0,0 +1,16 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Admin
+#
+# Created:     14-03-2024
+# Copyright:   (c) Admin 2024
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+#Electricity officer
+uc = int(input())
+cpu = int(input())
+tb = int(uc ** cpu)
+print(tb)
