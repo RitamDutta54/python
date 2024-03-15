d4818d376dda67f0a7e20b5a7b135678b5bef0c0
ritz54 <161623178+RitamDutta54@users.noreply.github.com>
2024-03-15 17:15:22 +0530

    Add files via upload

---
+++ vetrivel.py
@@ -0,0 +1,17 @@
+#-------------------------------------------------------------------------------
+# Name:        vetrivel
+# Purpose:
+#
+# Author:      #####
+#
+# Created:     15-03-2024
+# Copyright:   (c) Admin 2024
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+#vetrivel
+days=int(input())
+hours=int(input())
+minutes=int(input())
+seconds=int(input())
+print(days*86400+hours*3600+minutes*60+seconds)
